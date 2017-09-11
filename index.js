bfc330f042cef0dd7901cb719ba9765a4d25ebe1
Kyoto Kopz <kopz9999@gmail.com>
2017-09-11 12:49:51 -0700

    working version

---
+++ index.js
@@ -0,0 +1,33 @@
+#! /usr/bin/env node
+
+require('dotenv').config();
+var camelize = require('camelize');
+var fs = require('fs');
+var path = require('path');
+var appRoot = require('app-root-path');
+var environmentPath = path.join(appRoot.toString(), 'environment.json');
+var tsEnvironmentPath = path.join(appRoot.toString(), 'src', 'environments', 'base.ts');
+var currentValue, environmentBase, tsString;
+
+console.log('Reading base environment: ' + environmentPath);
+
+if (fs.existsSync(environmentPath)) {
+  environmentBase = JSON.parse(fs.readFileSync(environmentPath, 'utf-8').toString());
+} else {
+  environmentBase = {};
+}
+
+Object.keys(process.env).forEach(function (key) {
+  if (key.startsWith('NG_')) {
+    currentValue = process.env[key];
+    environmentBase[camelize(key.substr(3).toLowerCase())] = currentValue;
+  }
+});
+
+tsString = "export default " + JSON.stringify(environmentBase, null, '  ');
+
+console.log('Writing variables from process.env: ' + tsEnvironmentPath);
+
+fs.writeFileSync(tsEnvironmentPath, tsString);
+
+console.log('Finished');
